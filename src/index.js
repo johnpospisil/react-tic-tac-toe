7d00b9086ac759af8ca64d6d42a4eda2ef763f98
johnpospisil <audiofreak7@gmail.com>
2019-03-25 10:41:30 -0700

    Stop the game when there is a winner

--- src/index.js
+++ src/index.js
@@ -26,8 +26,11 @@ class Board extends React.Component {
 
     handleClick(i) {
         // 'slice()' creates a copy of the array each time a box is clicked.
-        // 
         const squares = this.state.squares.slice();
+        //  return early by ignoring a click if someone has won the game or if a Square is already filled:
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
@@ -86,6 +89,28 @@ class Game extends React.Component {
         );
     }
 }
+
+// Given an array of 9 squares, this function will check for a 
+// winner and return 'X', 'O', or null as appropriate.
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 
 // ========================================
 
